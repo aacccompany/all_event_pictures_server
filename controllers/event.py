9e1d4cbb7e8b441c7e832b17da21ee1f69397e03
PhuLookingGood <Boomza445533@gmail.com>
2025-07-26 13:27:31 +0700

    soft remove && protech endpoint

--- controllers/event.py
+++ controllers/event.py
@@ -2,13 +2,30 @@ from fastapi import APIRouter, Depends
 from schemas.event import EventResponse, EventCreate
 from sqlalchemy.orm import Session
 from core.database import get_db
-from services.event import EventService
-from typing import Annotated
+from services.event import EventService, EventUpdate
+from typing import Annotated, List
 from schemas.auth import UserResponse
 from middleware.auth import get_current_admin
+
 
 router = APIRouter()
 
 @router.post("/event", response_model=EventResponse)
 async def create_event(event: EventCreate, user:Annotated[UserResponse, Depends(get_current_admin)], db:Session = Depends(get_db)):
     return EventService(db).create_event(event, user) 
+
+@router.get("/events", response_model=List[EventResponse])
+async def get_events(db:Session = Depends(get_db)):
+    return EventService(db).get_events()
+
+@router.get("/event/{event_id}", response_model=EventResponse)
+async def get_event(event_id:int, db:Session = Depends(get_db)):
+    return EventService(db).get_event(event_id)
+
+@router.patch("/event/{event_id}", response_model=EventResponse)
+async def update_event(event_id:int, event:EventUpdate, user:Annotated[UserResponse, Depends(get_current_admin)], db:Session = Depends(get_db)):
+    return EventService(db).update_event(event_id, event, user)
+
+@router.delete("/event/{event_id}")
+async def remove_event(event_id:int, db: Session = Depends(get_db)):
+    return EventService(db).remove_event(event_id)
