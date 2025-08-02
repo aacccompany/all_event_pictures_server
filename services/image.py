96aa1b746bba93e2db006b18439ba4de6dce552f
PhuLookingGood <Boomza445533@gmail.com>
2025-08-02 12:49:31 +0700

    validate event_id

--- services/image.py
+++ services/image.py
@@ -1,18 +1,18 @@
 from sqlalchemy.orm import Session
 from repositories.image import ImageRepository
-from repositories.event import EventRepository
 from schemas.image import ImageUpload
 from schemas.auth import UserResponse
 from services.cloudinary import CloudinaryService
 from fastapi import UploadFile
-
+from services.event import EventService
 
 class ImageService:
     def __init__(self, db = Session):
         self.repo = ImageRepository(db)
-        self.repo_event = EventRepository(db)
+        self.event_service = EventService(db)
         
     async def upload_images(self, images: list[UploadFile], event_id:int, user: UserResponse):
+        self.event_service.get_event(event_id)
         upload_images = await CloudinaryService.upload_images(images, user)
         result = []
         for i in upload_images:
