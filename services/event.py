bae659ccf76090e1a30cbcd0b96f144a5a4c0eaa
PhuLookingGood <Boomza445533@gmail.com>
2025-08-07 09:07:06 +0700

    docker && event active.is_(True)

--- services/event.py
+++ services/event.py
@@ -38,7 +38,7 @@ class EventService:
     def search_events(self, title:str):
         events = self.repo.search_by_title(title)
         if not events:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Events not sound")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Events not found")
         return events
     
     def get_active_events(self):
