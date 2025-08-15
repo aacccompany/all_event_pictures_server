ea96c1dd8c2ef10b267c3ecae8ee7b300da20330
PhuLookingGood <Boomza445533@gmail.com>
2025-08-15 15:03:40 +0700

    Del cover_image

--- controllers/cloudinary.py
+++ controllers/cloudinary.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, File, Depends
 from services.cloudinary import CloudinaryService
-from fastapi import Depends
 from middleware.auth import get_current_admin
 from typing import Annotated
 from schemas.auth import UserResponse
@@ -8,7 +7,10 @@ from schemas.auth import UserResponse
 router = APIRouter()
 
 @router.post("/image")
-async def upload_image(image:UploadFile, user: Annotated[UserResponse, Depends(get_current_admin)]):
-    result = await CloudinaryService.upload_image(image, user)
+async def upload_image(
+    user: Annotated[UserResponse, Depends(get_current_admin)],  # ✅ ไม่มี default มาก่อน
+    image_cover: UploadFile = File(...)  # ✅ มี default มาทีหลัง
+):
+    result = await CloudinaryService.upload_image(image_cover, user)
     return result
-    
+
