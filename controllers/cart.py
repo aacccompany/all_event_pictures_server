b268af16b07280c340deb59a53c2a1985ec4fb67
PhuLookingGood <Boomza445533@gmail.com>
2025-09-06 21:44:29 +0700

    dowland

--- controllers/cart.py
+++ controllers/cart.py
@@ -5,9 +5,23 @@ from middleware.auth import get_current_user
 from services.cart import CartService
 from sqlalchemy.orm import Session
 from core.database import get_db
+from fastapi.responses import StreamingResponse
 
 router = APIRouter()
 
 @router.get("/my-cart", response_model=CartResponse)
 def get(user: UserResponse = Depends(get_current_user), db: Session = Depends(get_db)):
     return CartService(db).get_my_cart(user.id)
+
+@router.get("/my-cart/download")
+def download_my_cart(
+    user: UserResponse = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    zip_buffer, cart_id = CartService(db).download_cart_zip(user.id)
+
+    return StreamingResponse(
+        zip_buffer,
+        media_type="application/x-zip-compressed",
+        headers={"Content-Disposition": f"attachment; filename=cart_{cart_id}.zip"}
+    )
